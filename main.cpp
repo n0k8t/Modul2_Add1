4ea125d3f2d4be96c388bf0c5b6ea1802eeb9982
Timofey Tyrin <timat-ti@mail.ru>
2017-03-23 19:37:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 
-template<typename T>
-void print( T min, T max)
+template<size_t N>
+void print(size_t min = N)
 {
     std::cout << min << " ";
     min++;
-    if (min <= max)
-        print(min,max);
+
+    if (min <= 100)
+        print<N>(min);
 }
 
-int main() {
-    print(0,100);
+
+int main()
+{
+    print<0>();
 }
